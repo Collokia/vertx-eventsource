bd3d945530bbc637cc54162411c44638f40fa48d
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2015-09-07 19:19:49 +0300

    Async consumer verticle init

--- src/main/kotlin/org/collokia/vertx/eventsource/EventSourceConsumerVerticle.kt
+++ src/main/kotlin/org/collokia/vertx/eventsource/EventSourceConsumerVerticle.kt
@@ -1,7 +1,10 @@
 package org.collokia.vertx.eventsource
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
 import io.vertx.core.json.JsonObject
+import io.vertx.core.logging.LoggerFactory
+import nl.komponents.kovenant.async
 import org.glassfish.jersey.media.sse.EventSource
 import org.glassfish.jersey.media.sse.SseFeature
 import javax.ws.rs.client.ClientBuilder
@@ -9,25 +12,37 @@ import kotlin.properties.Delegates
 
 class EventSourceConsumerVerticle : AbstractVerticle() {
 
+    companion object {
+        val log = LoggerFactory.getLogger("EventSourceConsumerVerticle")
+    }
+
     var eventSource: EventSource by Delegates.notNull()
 
-    override fun start() {
-        val client = ClientBuilder.newBuilder().register(javaClass<SseFeature>()).build()
+    override fun start(startFuture: Future<Void>) {
+        async {
+            val encoding    = config().getString("eventConsumer.encoding") ?: "UTF-8"
+            val endpointURI = config().getString("eventConsumer.endpointURI")
+            val address     = config().getString("eventConsumer.address")
 
-        val encoding    = config().getString("eventConsumer.encoding") ?: "UTF-8"
-        val endpointURI = config().getString("eventConsumer.endpointURI")
-        val address     = config().getString("eventConsumer.address")
+            log.info("Trying to start EventSource consumer of $endpointURI")
 
-        assert(endpointURI != null, "'eventConsumer.endpointURI' can't be empty")
-        assert(address != null,  "'eventConsumer.address' can't be empty")
+            val client = ClientBuilder.newBuilder().register(javaClass<SseFeature>()).build()
 
-        val webTarget = client.target(endpointURI)
-        eventSource = EventSource(webTarget)
-        eventSource.register { event ->
-            val dataJson = JsonObject(String(event.getRawData(), encoding))
-            vertx.eventBus().send(address, dataJson)
+            assert(endpointURI != null, "'eventConsumer.endpointURI' can't be empty")
+            assert(address != null, "'eventConsumer.address' can't be empty")
+
+            val webTarget = client.target(endpointURI)
+            eventSource = EventSource(webTarget)
+            eventSource.register { event ->
+                val dataJson = JsonObject(String(event.getRawData(), encoding))
+                vertx.eventBus().send(address, dataJson)
+            }
+
+            log.info("Started EventSource consumer of $endpointURI")
+            startFuture.complete()
+
+            eventSource.open()
         }
-        eventSource.open()
     }
 
     override fun stop() {
