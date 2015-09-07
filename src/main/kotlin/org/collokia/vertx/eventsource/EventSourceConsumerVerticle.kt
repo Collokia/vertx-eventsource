33dae9f326a354de0075b10d21422abcc4f5ebfb
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2015-09-07 23:38:50 +0300

    Event types support

--- src/main/kotlin/org/collokia/vertx/eventsource/EventSourceConsumerVerticle.kt
+++ src/main/kotlin/org/collokia/vertx/eventsource/EventSourceConsumerVerticle.kt
@@ -5,6 +5,7 @@ import io.vertx.core.Future
 import io.vertx.core.json.JsonObject
 import io.vertx.core.logging.LoggerFactory
 import nl.komponents.kovenant.async
+import org.glassfish.jersey.media.sse.EventListener
 import org.glassfish.jersey.media.sse.EventSource
 import org.glassfish.jersey.media.sse.SseFeature
 import javax.ws.rs.client.ClientBuilder
@@ -18,8 +19,10 @@ class EventSourceConsumerVerticle : AbstractVerticle() {
 
     var eventSource: EventSource by Delegates.notNull()
 
+
     override fun start(startFuture: Future<Void>) {
         async {
+            val eventTypes  = config().getJsonArray("eventConsumer.eventTypes")?.map { it as? String }?.filterNotNull().orEmpty()
             val encoding    = config().getString("eventConsumer.encoding") ?: "UTF-8"
             val endpointURI = config().getString("eventConsumer.endpointURI")
             val address     = config().getString("eventConsumer.address")
@@ -33,9 +36,21 @@ class EventSourceConsumerVerticle : AbstractVerticle() {
 
             val webTarget = client.target(endpointURI)
             eventSource = EventSource(webTarget)
-            eventSource.register { event ->
+
+
+            val eventListener: EventListener = EventListener { event ->
                 val dataJson = JsonObject(String(event.getRawData(), encoding))
                 vertx.eventBus().send(address, dataJson)
+            }
+
+            if (eventTypes.isEmpty()) {
+                eventSource.register(eventListener)
+            } else {
+                if (eventTypes.size() > 1) {
+                    eventSource.register(eventListener, eventTypes.first(), * eventTypes.drop(1).copyToArray())
+                } else {
+                    eventSource.register(eventListener, eventTypes.first())
+                }
             }
 
             log.info("Started EventSource consumer of $endpointURI")
